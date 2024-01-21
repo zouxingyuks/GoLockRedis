60b349b3c19bfc723d71656d3835e36b31ccda40
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2024-01-21 17:36:31 +0800

    支持基本锁的版本，读写锁尚未支持

--- pool.go
+++ pool.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"github.com/go-redis/redis/v8"
 )
 
@@ -10,16 +9,33 @@ type Pool struct {
 	client *redis.Client
 }
 
+// keyGen 生成锁的key
+type keyGen func(lockKey string) string
+
+// DefaultKeyGen 默认的key生成器
+var DefaultKeyGen = func(lockKey string) string {
+	return "grlock:" + lockKey
+}
+
 func NewPool(client *redis.Client) *Pool {
-	return &Pool{client: client}
+	return &Pool{
+		client: client,
+	}
 }
 
 // NewMutex 创建一个分布式锁
 // client redis客户端
 // lockKey 锁的key
+func (p *Pool) NewMutex(lockKey string, gen keyGen, opts ...Option) Mutex {
+	m := newMutex(p.client, gen(lockKey), "", opts...)
+	return m
+}
+
+// NewRWMutex 创建一个分布式读写锁
+// client redis客户端
+// lockKey 锁的key
 // lockValue 锁的value（用于实现可重入式锁）
-func (p *Pool) NewMutex(ctx context.Context, lockKey string, lockValue any, opts ...Option) Mutex {
-	//todo 支持自定义uuid
-	m := newMutex(p.client, lockKey, lockValue, opts...)
+func (p *Pool) NewRWMutex(lockKey string, opts ...Option) RWMutex {
+	m := newRWMutex(p.client, lockKey, opts...)
 	return m
 }
