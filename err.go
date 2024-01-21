5f3a353297c562007b076899c3bc1b53e16a4a84
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2024-01-21 17:37:51 +0800

    补充 解锁失败的错误

--- err.go
+++ err.go
@@ -6,4 +6,5 @@ var (
 	//ErrMutexHasLocked 锁已经被占用
 	ErrMutexHasLocked = errors.New("mutex has locked")
 	ErrRefreshFailed  = errors.New("refresh failed")
+	ErrUnlockFailed   = errors.New("unlock failed")
 )
