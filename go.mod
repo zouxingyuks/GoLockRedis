1fd38c27d207e1aca88b267149a241b6b04c8634
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2024-01-22 17:45:08 +0800

    init

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/zouxingyuks/GoLockRedis
 
-go 1.21
+go 1.21.0
 
 require (
 	github.com/go-redis/redis/v8 v8.11.5
@@ -10,6 +10,4 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
 )
--- metux_test.go
+++ metux_test.go
@@ -1,10 +1,14 @@
 package goredislock
 
 import (
+	"context"
+	"fmt"
 	"github.com/go-redis/redis/v8"
+	"github.com/pkg/errors"
 	"testing"
 )
 
+// 测试正常的加锁和解锁
 func Test_mutex_Lock_Unlock(t *testing.T) {
 	tests := []struct {
 		name string
@@ -18,10 +22,11 @@ func Test_mutex_Lock_Unlock(t *testing.T) {
 
 	pool := NewPool(redis.NewClient(&redis.Options{
 		Addr: "localhost:6379",
-	}))
+	}), DefaultKeyGen, DefaultValueGen)
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			m := pool.NewMutex(tt.name, DefaultKeyGen)
+			ctx := context.WithValue(context.Background(), "gid", "test1")
+			m := pool.NewMutex(ctx, tt.name)
 			_, err := m.Lock()
 			if err != nil {
 				t.Errorf("Lock() error = %v", err)
@@ -33,6 +38,47 @@ func Test_mutex_Lock_Unlock(t *testing.T) {
 				t.Errorf("Unlock() error = %v", err)
 				return
 			}
+		})
+	}
+}
+
+// 测试重复加锁
+func Test_mutex_repeat_Lock(t *testing.T) {
+	tests := []struct {
+		name string
+		gid  string
+	}{
+		{
+			name: "test1",
+			gid:  "test1",
+		},
+	}
+
+	pool := NewPool(redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	}), DefaultKeyGen, DefaultValueGen)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.WithValue(context.Background(), "gid", tt.gid)
+			wantGid := DefaultValueGen(ctx)
+			m := pool.NewMutex(ctx, tt.name)
+			_, err := m.Lock()
+			if err != nil {
+				t.Errorf("Lock() error = %v", err)
+				return
+			}
+			fmt.Println("第一次加锁成功")
+			gid, err := m.Lock()
+			if err != nil && !errors.Is(err, ErrMutexHasLocked) {
+				t.Errorf("Lock() error = %v", err)
+				return
+			}
+			if wantGid != gid {
+				t.Errorf("gid is %v, want %v", gid, DefaultValueGen(ctx))
+				return
+			}
+			fmt.Println("重复加锁成功")
+
 		})
 	}
 }
